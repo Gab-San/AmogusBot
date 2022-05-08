ca96cd98eb95e5d28ac7d4613234fd8eaacb489d
LoZack19 <giozaccaria02@gmail.com>
2022-05-08 22:35:47 +0200

    sus

--- amogusBot.py
+++ amogusBot.py
@@ -8,3 +8,7 @@ TOKEN = "OTcyOTUzODkwNzQ2MTQ2ODc2.GwXdcC.7CsNQtPt3bBhK6e6eje30CH2f6oqLIm7_8ryoY"
 # Create Bot
 
 client = commands.Bot(commands_prefix = "!")
+
+@client.command()
+async def sussybaka(ctx):
+    ctx.send("sus")
