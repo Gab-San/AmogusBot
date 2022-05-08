5ec2d965a947553bb260b3aa19c8e175ce8b15bd
LegLord <santandreagabriele@gmail.com>
2022-05-08 22:54:19 +0200

    + Added whosesus command that responds with "You're SUS text"

--- amogusBot.py
+++ amogusBot.py
@@ -3,12 +3,29 @@ from discord.ext import commands
 
 # Credentials
 
-TOKEN = "OTcyOTUzODkwNzQ2MTQ2ODc2.GwXdcC.7CsNQtPt3bBhK6e6eje30CH2f6oqLIm7_8ryoY"
+TOKEN = "OTcyOTUzODkwNzQ2MTQ2ODc2.GtqFHT.Bn0RHuR--MkUCBa2-XrSTvy4w1KJYu8kE1fp98"
 
 # Create Bot
 
-client = commands.Bot(commands_prefix = "!")
+client = commands.Bot(command_prefix = "!")
+
+# On Start
+
+@client.event
+async def on_ready():
+    print(" Connected to bot: {}".format(client.user.name))
+    print(" Bot ID: {}".format(client.user.id)) 
+
 
 @client.command()
-async def sussybaka(ctx):
-    ctx.send("sus")
+async def sus(ctx):
+    await ctx.send("sus")
+
+@client.command()
+async def whosesus(ctx):
+    await ctx.send("You're SUS")
+
+
+# Run the Bot
+
+client.run(TOKEN)
