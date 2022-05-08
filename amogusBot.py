f5f70921607aeea59831456102f997617a9e116d
LegLord <santandreagabriele@gmail.com>
2022-05-08 22:28:26 +0200

    Initial Commit

---
+++ amogusBot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+
+# Credentials
+
+TOKEN = "OTcyOTUzODkwNzQ2MTQ2ODc2.GwXdcC.7CsNQtPt3bBhK6e6eje30CH2f6oqLIm7_8ryoY"
+
+# Create Bot
+
+client = commands.Bot(commands_prefix = "!")
